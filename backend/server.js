4c2a65b2f84d01e847f0f5475664c8433cf74cfa
pavantejareddy1 <pavantejareddy85@gmail.com>
2026-05-07 18:34:19 +0530

    Updated backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
+
 const authRoutes = require("./routes/authRoutes");
 const testRoutes = require("./routes/testRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
-
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-})
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
 );
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Backend Running Successfully");
-});
+app.listen
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => { console.log("Connected to DB") })
-    .catch((err) => { console.log(err.message) });
+    .then(() => {console.log("Connected to DB")})
+    .catch((error) => {console.log(error);});
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
+module.exports = app;
