7c15a0b80c235f8c93ffb442f3dce8de840566fb
pavantejareddy1 <pavantejareddy85@gmail.com>
2026-05-07 19:00:41 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,11 @@ const app = express();
 app.use(
     cors({
         origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
-
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/projects", projectRoutes);
