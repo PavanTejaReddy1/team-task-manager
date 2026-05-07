fe844f9b79e64cec9e1a07cf760f0bc8362ce5e0
pavantejareddy1 <pavantejareddy85@gmail.com>
2026-05-07 18:58:57 +0530

    Added default route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
